12fae9b58fcd38acbf116ff9ad73d3fff3c249de
Seth Nelson <seth.nelson@outlook.com>
2020-03-17 22:32:13 -0700

    finished styling

--- github-usercard/src/Components/User.js
+++ github-usercard/src/Components/User.js
@@ -4,13 +4,13 @@ import React from 'react';
 function User(props) {
     console.log('user data return', props)
     return (
-        <div className='user-card'>
-            <a href={props.url} className='user-card'>
+            <div className='user-card'>
                 <img src={props.image} alt={props.header} />
                 <h3>{props.header}</h3>
-                <p>{props.url}</p>
-            </a>
-        </div>
+                <a href={props.url}>
+                    <p>{props.url}</p>
+                </a>
+            </div>
     );
 }
 
